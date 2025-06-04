85f4535960cb006329925b78c29cbead293346e0
Ashique64 <ashiquemon61@gmail.com>
2025-06-04 11:56:52 +0530

    Home page design completed

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -7,7 +7,31 @@ const Home = () => {
     <>
       <NavBar />
       <div className="home">
-        
+        <div className="container">
+          <div className="row title-row">
+            <div className="col-md-12 title-col">
+              <h2>Send mail to your client</h2>
+            </div>
+          </div>
+
+          <div className="row sent-mail-row">
+            <div className="col-md-12 sent-mail-col">
+              <div className="row mail-items-row">
+                <div className="col-md-6 col-lg-8 col_1">
+                  <input
+                    className="form-control"
+                    type="email"
+                    placeholder="Enter Your Client Email"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 col-lg-2 col_2">
+                  <button className="btn">Send</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
